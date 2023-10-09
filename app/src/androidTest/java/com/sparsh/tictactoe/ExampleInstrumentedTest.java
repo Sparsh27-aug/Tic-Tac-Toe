0bb58895197cfffb680b634b549dcfd38b8e41da
ishita0906 <70266080+ishita0906@users.noreply.github.com>
2023-10-09 21:53:07 +0530

    Update ExampleInstrumentedTest

--- app/src/androidTest/java/com/sparsh/tictactoe/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/sparsh/tictactoe/ExampleInstrumentedTest.java
@@ -8,7 +8,7 @@ import androidx.test.ext.junit.runners.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static org.junit.Assertion;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.sparsh.tictactoe", appContext.getPackageName());
+        Assertion.assertEquals("com.sparsh.tictactoe", appContext.getPackageName());
     }
 }
